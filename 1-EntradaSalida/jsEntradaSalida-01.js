937b4d62523a7451793271375ad1c80e37b22383
Lucas Chiavello <LucasChiavello>
2020-07-14 12:08:39 -0300

    Primera Clase

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,8 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola UTN");
+	alert("Hola UTN");  // Una alerta
+	confirm("Desea Continuar?")  // Una alerta de confirmacion
+	prompt("Ingrese su Nombre")  // Alerta para tomar informacion
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,18 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var dato;
+
+	dato = prompt("Ingrese un Dato");
+
+	alert(dato);
+
+	// alert(prompt("ingrese un dato"));  esto no
+	// var nombre;
+	// nombre = "Lucas"; // string
+	// nombre = null; // se usa para anular o quitar el string de la variable
+	// nombre = 12; // dato de tipo number o numero
+	// nombre = true; // boolean solo pueden dar true o false
+	// alert(nombre)
 }
 
